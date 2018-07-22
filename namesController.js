e65580a62984573f1409b5fe4b8107c4719f3e63
NahuelEz2 <nec2@live.com.ar>
2018-07-22 18:34:59 -0300

    Add files via upload

---
+++ namesController.js
@@ -0,0 +1,7 @@
+angular.module('myApp', []).controller('namesCtrl', function($scope) {
+    $scope.names = [
+        {name:'Nahuel',country:'Correa'},
+        {name:'Mayra',country:'Stevenson'},
+        {name:'Brisa',country:'Conrad'}
+    ];
+});
